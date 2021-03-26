f4ee46db71cf63a133b041c0d2febd8458c5c120
Golamrabbani-ux <golamrabbani227417@gmail.com>
2021-03-26 16:55:00 +0600

    page post route add

--- src/routes/admin/page.js
+++ src/routes/admin/page.js
@@ -3,7 +3,7 @@ const { upload, requireSignin } = require('../../common-middlwear');
 const { page, getPage } = require('../../controller/admin/page');
 const route = express.Router();
 
-// route.post('/page', upload.fields([{name: "banners"},{name: "productsPictures"}]), requireSignin, page);
+route.post('/page', upload.fields([{name: "banners"},{name: "productsPictures"}]), requireSignin, page);
 route.get('/page/:category/:type', getPage)
 
 
