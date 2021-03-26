0021183ae09f8c8a68a5e9e9a6ace0effa079b0c
Golamrabbani-ux <golamrabbani227417@gmail.com>
2021-03-26 18:26:31 +0600

    page add route

--- src/controller/admin/page.js
+++ src/controller/admin/page.js
@@ -2,41 +2,37 @@ const Page = require('../../models/page');
 const User = require('../../models/auth');
 
 exports.page = (req, res) => {
-    User.findById({ _id: req.user._id })
-        .exec((error, user) => {
-            if (error) return res.status(400).json({ error })
-            if (user.role === "admin") {
-                const { banners } = req.files;
-                if (banners.length > 0) {
-                    req.body.banners = banners.map((banner) => ({
-                        img: banner.filename,
-                    }))
-                }
-                req.body.createdBy = req.user._id;
-                res.status(201).json({body: req.body})
-                Page.findOne({ category: req.body.category })
-                    .exec((err, page) => {
-                        if (err) return res.status(200).json({ err });
-                        else if (page) {
-                            Page.findOneAndUpdate({ category: req.body.category }, req.body)
-                                .exec((_err, updatePage) => {
-                                    if (_err) return res.status(200).json({ _err });
-                                    return res.status(201).json({ updatePage })
-                                })
-                        }
-                        else {
-                            const page = new Page(req.body)
-                            page.save((_error, page) => {
-                                if (_error) return res.status(400).json({ _error });
-                                res.status(201).json({ page })
-                            })
-                        }
-                    })
+    const { banners, products } = req.files;
+    if (banners && banners.length > 0) {
+      req.body.banners = banners.map((banner, index) => ({
+        img: banner.filename
+      }));
+    }
+
+    req.body.createdBy = req.user._id;
+
+    Page.findOne({ category: req.body.category }).exec((error, page) => {
+      if (error) return res.status(400).json({ error });
+      if (page) {
+        Page.findOneAndUpdate({ category: req.body.category }, req.body).exec(
+          (error, updatedPage) => {
+            if (error) return res.status(400).json({ error });
+            if (updatedPage) {
+              return res.status(201).json({ page: updatedPage });
             }
-            else {
-                return res.status(400).json({ message: "Admin access only" })
-            }
-        })
+          }
+        );
+      } else {
+        const page = new Page(req.body);
+  
+        page.save((error, page) => {
+          if (error) return res.status(400).json({ error });
+          if (page) {
+            return res.status(201).json({ page });
+          }
+        });
+      }
+    });
 }
 
 exports.getPage = (req, res) => {
